0af35f4f9fc8ac8aed938f22e1d835a84944383c
Abhijit <abhijit@Abhijits-Mac.local>
2013-10-07 23:06:44 +0800

    First Commit

---
+++ ObjCPractice/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ObjCPractice
+//
+//  Created by Abhijit on 7/10/13.
+//  Copyright (c) 2013 Abhijit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
